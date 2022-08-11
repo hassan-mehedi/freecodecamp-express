bcc6c7a1933e4d71804dc1a5d7547472414d256b
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2022-08-11 23:24:43 +0600

    add middleware for all the routes

--- myApp.js
+++ myApp.js
@@ -6,6 +6,10 @@ console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+});
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
